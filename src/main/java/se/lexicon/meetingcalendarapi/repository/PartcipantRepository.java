d87458cb10559ac1399529857f853901a75d40ca
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 20:58:13 +0200

    updated  repository to find all participants by setof emails

--- src/main/java/se/lexicon/meetingcalendarapi/repository/PartcipantRepository.java
+++ src/main/java/se/lexicon/meetingcalendarapi/repository/PartcipantRepository.java
@@ -1,13 +1,17 @@
 package se.lexicon.meetingcalendarapi.repository;
 
-import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.lexicon.meetingcalendarapi.domain.entity.Participant;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface PartcipantRepository extends JpaRepository<Participant,String> {
+
+
+    List<Participant> findAllByEmailIn(Set<String> emails);
 
 }
