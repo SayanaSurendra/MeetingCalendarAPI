2b52e7f06fc744f54c03dfc20d7b8f610f330721
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 20:56:45 +0200

    updated the relationships for entity

--- src/main/java/se/lexicon/meetingcalendarapi/domain/entity/MeetingCalendar.java
+++ src/main/java/se/lexicon/meetingcalendarapi/domain/entity/MeetingCalendar.java
@@ -15,24 +15,36 @@ import java.util.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
-@EqualsAndHashCode
 @Builder
 public class MeetingCalendar {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer meetingId;
+
+    @Column(nullable = false)
     String meetingTitle;
+
+    @Column(nullable = false)
     String meetingLevel;
+
+    @Column(nullable = false)
     String meetingDescription;
+
+    @Column(nullable = false)
     LocalDate meetingDate;
+
+    @Column(nullable = false)
     LocalTime meetingTime;
 
-    @OneToMany
-    @JoinColumn(name = "meeting_id")
+    @ToString.Exclude
+    @ManyToMany
+    @JoinTable(
+            name = "meeting_participants",
+            joinColumns = @JoinColumn(name = "meeting_id"),
+            inverseJoinColumns = @JoinColumn(name = "email")
+    )
     Set<Participant> participants = new HashSet<>();
-
-
 
 
     public MeetingCalendar(String meetingTitle,String meetingDescription,String meetingLevel, LocalDate meetingDate, LocalTime meetingTime, Set<Participant> participants) {
@@ -43,5 +55,17 @@ public class MeetingCalendar {
         this.participants = participants;
         this.meetingLevel = meetingLevel;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MeetingCalendar that = (MeetingCalendar) o;
+        return Objects.equals(meetingId, that.meetingId) && Objects.equals(meetingTitle, that.meetingTitle) && Objects.equals(meetingLevel, that.meetingLevel) && Objects.equals(meetingDescription, that.meetingDescription) && Objects.equals(meetingDate, that.meetingDate) && Objects.equals(meetingTime, that.meetingTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(meetingId, meetingTitle, meetingLevel, meetingDescription, meetingDate, meetingTime);
     }
 }
